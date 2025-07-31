519ae371271a2f22c5c6e74fba8717dee111aee9
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-31 20:30:47 +0530

    working app

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(['task_msg'])
+
+const show = ref(true)
+</script>
+
+<template>
+  <div v-if="show" class="task">
+    <input class="tick" type="checkbox" name="" id="" />
+    <p>{{ task_msg }}</p>
+    <button class="remove-btn" @click="show = false">Remove</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.task {
+  margin: 1rem;
+  width: 100%;
+  border-radius: 10px;
+  padding: 0.3rem 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0.4rem;
+  font-size: 2rem;
+  background: var(--blue);
+
+  .tick {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  p {
+    margin: 0;
+    text-align: center;
+    font-size: 2rem;
+    width: 30rem;
+    word-break: break-word;
+  }
+  .remove-btn {
+    margin: 0;
+    border-radius: 8px;
+    font-size: 1.2rem;
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,63 @@
-<script setup></script>
+<script setup>
+import Task from '@/components/Task.vue'
+
+import { ref } from 'vue'
+
+const task = ref('')
+const tasks = ref([])
+</script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <h1>To-Do App</h1>
+    <div class="add-task">
+      <input v-model="task" type="text" placeholder="Add a new task" />
+      <button @click="tasks.push(task)">Add Task</button>
+    </div>
+    <div class="tasks" v-for="(task, index) in tasks.reverse()" :key="index">
+      <Task :task_msg="task" />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-h1 {
-  text-align: center;
-  color: #42b983;
-  font-size: 5rem;
+div.container {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 4rem;
+    font-family: sans;
+    color: var(--blue);
+  }
+
+  .add-task {
+    margin-top: 2rem;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    border-radius: 1rem;
+    input {
+      width: 80%;
+      font-size: 2rem;
+      padding: 0.5rem;
+    }
+    button {
+      width: 20%;
+      font-size: 1.5rem;
+      padding: 0.5rem;
+    }
+  }
+
+  .tasks {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    align-items: center;
+  }
 }
 </style>
