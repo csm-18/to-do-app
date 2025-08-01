7a66a724442c0961221a22f3bc8fe8c784c15761
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-08-02 05:11:56 +0530

    gh-pages config

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(), //createWebHistory(import.meta.env.BASE_URL)
 
   routes: [
     {
